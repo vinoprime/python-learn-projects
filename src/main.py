db4193f510f7b3ec115d21df32f5a99fae295415
Vinoth <vinothnayagar@gmail.com>
2023-10-27 21:44:57 +0530

    basics

--- src/main.py
+++ src/main.py
@@ -1,1 +1,65 @@
-print("Hello")
+# name = input("Enter:")
+# print("Hello"+name)
+
+# Data Types
+int # 12
+float # 65.25
+bool # True/false
+str # "Hello String"
+list # []
+tuple
+set # ()
+dict # []
+None # absence of value/ means nothing
+
+
+#  Classes -> Custom types
+# Specialized Data Types -> Modules
+
+
+# int
+print(1+2)
+print(1-2)
+print(1 * 2)
+print(1 / 2)
+print(2 ** 2)
+print(2 // 4)
+print(6 % 4)
+print(type(3))
+# float
+print(1.5)
+print(type(1.5))
+
+# bool
+print(True)
+# str
+print("Hello str")
+
+
+
+# Math
+print(round(1.26))
+print(abs(-12))
+
+# Operator Precedence
+print(20+3*4) # 20+(3*4)
+
+# ()
+# **
+# * /
+# + -
+
+# Complex
+print(complex(1,2.0)) # (1+2j)
+
+
+print(bin(5)) #0b101
+print(oct(5))
+print(hex(5))
+
+print(int("0b101", 2))
+
+
+
+
+
