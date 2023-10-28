c20a790aedb567ea63c1c6fa4af0ef5c4f4d6ac7
Vinoth <vinothnayagar@gmail.com>
2023-10-28 14:47:11 +0530

    steps

---
+++ src/ml.py
@@ -0,0 +1,7 @@
+print("Hello ml")
+# 1. import the data
+# 2. clean the data
+# 3. Split the data (Traning Set 80% of data / Test Set 20% of data)
+# 4. Create a Model
+# 5. Check the output
+# 6. Improve
