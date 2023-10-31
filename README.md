482845145c018a37ab0e7bec637af93b56b3572d
Vinoth <vinothnayagar@gmail.com>
2023-10-31 06:10:42 +0530

    added jypyter extention

--- README.md
+++ README.md
@@ -17,3 +17,34 @@ python -m pip install --upgrade pip
 $ pip install -r .\requirements.txt
 # to copy all installations in .txt file
 $ pip freeze > requirements.txt
+
+
+
+
+# Bellow packages needed for jypyter notebook to run in vscode
+asttokens==2.4.1
+colorama==0.4.6
+comm==0.1.4
+debugpy==1.8.0
+decorator==5.1.1
+executing==2.0.1
+ipykernel==6.26.0
+ipython==8.17.1
+jedi==0.19.1
+jupyter_client==8.5.0
+jupyter_core==5.5.0
+matplotlib-inline==0.1.6
+nest-asyncio==1.5.8
+parso==0.8.3
+platformdirs==3.11.0
+prompt-toolkit==3.0.39
+psutil==5.9.6
+pure-eval==0.2.2
+Pygments==2.16.1
+pywin32==306
+pyzmq==25.1.1
+stack-data==0.6.3
+tornado==6.3.3
+traitlets==5.13.0
+wcwidth==0.2.9
+# packages needed for jypyter notebook to run in vscode
--- src/ml.py
+++ src/ml.py
@@ -1,66 +1,6 @@
-print("Hello ml")
 # 1. import the data
 # 2. clean the data
 # 3. Split the data (Traning Set 80% of data / Test Set 20% of data)
 # 4. Create a Model
 # 5. Check the output
 # 6. Improve
-
-
-
-
-
-#  We'll create a basic model to predict student scores based on the number of hours they study.
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Sample data: Number of hours studied and corresponding exam scores
-hours_studied = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-exam_scores = np.array([25, 45, 60, 70, 80, 85, 88, 92, 95, 97])
-
-# Calculate the mean of hours studied and exam scores
-mean_hours = np.mean(hours_studied)
-mean_scores = np.mean(exam_scores)
-
-
-#### Formula-1 theata1 = sumation(∑) of i=1 to n (xi-x^)(yi-y^)/ sumation(∑) of i=1 to n (xi-x^)^2
-# theata1 => represents the slope of the regression line.
-# xi and yi => represents are individual data points in your dataset.
-# x^ and y^ => the mean (average) of the x-values and y-values, respectively.
-# The summation symbol (∑) indicates that you should perform the calculations for each data point and then sum them up.
-#### Formula-2 theata0 = y^ - theata1 * x^
-# theata0 => represents the y-intercept of the regression line.
-#  y^ and x^ => are the means of the x-values and y-values.
-
-
-
-# Calculate the slope (theta1) and y-intercept (theta0) of the regression line
-numerator = np.sum((hours_studied - mean_hours) * (exam_scores - mean_scores))
-denominator = np.sum((hours_studied - mean_hours) ** 2)
-theta1 = numerator / denominator
-theta0 = mean_scores - theta1 * mean_hours
-
-# Create the regression line
-regression_line = theta0 + theta1 * hours_studied
-
-print(theta0,theta1,hours_studied)
-mean_hours
-
-
-# Plot the data points
-plt.scatter(hours_studied, exam_scores, label='Actual Scores')
-
-# Plot the regression line
-plt.plot(hours_studied, regression_line, color='red', label='Regression Line')
-
-# Customize the plot
-plt.xlabel('Hours Studied')
-plt.ylabel('Exam Scores')
-plt.title('Linear Regression in Education')
-plt.legend()
-plt.grid()
-
-# Show the plot
-plt.show()
