87dffaa7ce6db8f618054a1d2255d4b2db891abf
Vinoth <vinothnayagar@gmail.com>
2023-11-19 23:24:56 +0530

    ml

---
+++ src/ml/haker_earth/prob.py
@@ -0,0 +1,14 @@
+# Baye's theorem
+
+b1 = 30
+b2 = 45
+b3 = 25
+
+b1_defect = 2
+b2_defect = 3
+b3_defect = 2
+
+
+p_a = (b1/100*b1_defect/100)+(b2/100*b2_defect/100)+(b3/100*b3_defect/100)
+print(p_a)
+
